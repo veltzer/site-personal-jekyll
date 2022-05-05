5aa4f2c9ab5ef661a0a1f93766870fd2380cafd7
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 01:19:14 +0300

    fixing build

--- config/deps.py
+++ config/deps.py
@@ -1,3 +1,4 @@
 packages=[
-    "pymakehelper",
+    "ruby-bundler",
+    "rbenv",
 ]
---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "site-personal-jekyll"
+project_long_description = "A personal site for me in jekyll"
+project_year_started = 2017
+project_description = """A personal site for me in jekyll"""
