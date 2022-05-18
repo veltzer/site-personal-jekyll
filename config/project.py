c93e87337fb80b20e83cd559ff1720f3cbf84bcd
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:13 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "site-personal-jekyll"
-project_long_description = "A personal site for me in jekyll"
-project_year_started = 2017
-project_description = """A personal site for me in jekyll"""
+description_short = "A personal site for me in jekyll"
